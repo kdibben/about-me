1f416a734b5de601a443a88978ce2afb2676739d
Kelsie Dibben <kmdibben@gmail.com>
2019-10-10 15:00:41 -0500

    Working api call that fetches random cat fact

---
+++ scripts/fetch-cat-fact.js
@@ -0,0 +1,31 @@
+const catFactURL = 'https://cors-anywhere.herokuapp.com/https://catfact.ninja/fact'
+
+// fetch information
+const getFact = async () => {
+  try {
+    const response = await fetch(catFactURL)
+    const obj = await response.json()
+    console.log(`Fetched: ${obj}`)
+    const fact = obj.fact
+    return fact
+  } catch (error) { 
+      console.error(error) 
+    }
+}
+
+// interact with DOM
+const updateWithFact = async (event) => {
+  try {
+    document.getElementById('catFact').innerHTML = ''
+    const result = await getFact()
+    document.getElementById('catFact').innerHTML = result
+  } catch (error) { 
+      console.error(error) 
+    }
+}
+
+document.addEventListener('click', event => {
+    if (event.target && event.target.id === 'getCatFact') { 
+        updateWithFact(event) 
+    }
+  })
